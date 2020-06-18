a53b86597f207ea6757bd21ec1a1ef9d38ea832b
Jatin <jatinmadaan9882@gmail.com>
2020-06-18 11:29:26 +0530

    Reactive Form Validation on Zip Code

--- src/app/forms/reactive-sample/reactive-sample.component.ts
+++ src/app/forms/reactive-sample/reactive-sample.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-sample',
@@ -15,24 +15,29 @@ export class ReactiveSampleComponent implements OnInit {
 
   ngOnInit() {
 
-    console.log('Initial Value', this.user)
+    //console.log('Initial Value', this.user)
 
     this.reactiveForm = this._fb.group({
-      firstName: [this.user.fname],
-      lastName: [this.user.lname],
+      firstName: ['', [Validators.required, Validators.minLength(2)]],
+      lastName: ['', [Validators.required, Validators.minLength(2)]],
       address: this._fb.group({
-        addressType : [],
+        addressType : [null, Validators.required],
         expiryDate : [],
-        streetAddress : [],
-        city : [],
-        state : [],
-        zipcode : []
+        streetAddress : [null, Validators.required],
+        city : [null, Validators.required],
+        state : [null, Validators.required],
+        zipcode : [null, [Validators.required, Validators.pattern("^[1-9][0-9]{5}")]]
       })
     });
   }
 
   submitHandler(){
     console.log(this.reactiveForm);
-    console.log('Final Value', this.user)
+    //console.log('Final Value', this.user)
+  }
+
+  get zipcode() { 
+    const temp = <FormGroup>this.reactiveForm.controls.address;
+    return temp.controls.zipcode;
   }
 }
